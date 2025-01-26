2effc37f80233fce3641418e6789c62ae2f4be79
NickZagotsis <nickzagotsis@gmail.com>
2025-01-26 16:58:14 +0200

    ?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
     @latest_posts = Post.order(created_at: :desc).limit(3)
-    @latest_private_rooms = Room.where(room_type: "private").order(created_at: :desc).limit(3)
+    @latest_private_rooms = Room.where(room_type: "private").where("name LIKE ?", "%#{current_user.email}%").order(created_at: :desc).limit(3)
   end
 end
